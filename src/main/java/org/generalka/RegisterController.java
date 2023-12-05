7224b8b3425aab79a64a7e99b76ed1abb4b8f843
Adam_Bruno Balint <adambrunobalint@gmail.com>
2023-12-05 07:25:48 +0100

    RegisterController add

--- src/main/java/org/generalka/RegisterController.java
+++ src/main/java/org/generalka/RegisterController.java
@@ -1,4 +1,37 @@
 package org.generalka;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RegisterController {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Label wrongUsername;
+
+    @FXML
+    void moveToLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userLogin(ActionEvent event) {
+
+    }
+
 }
