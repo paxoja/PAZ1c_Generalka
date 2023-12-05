9b1c327d7d156d6a13a7c7eb44628321cf73916a
Adam_Bruno Balint <adambrunobalint@gmail.com>
2023-12-05 06:48:53 +0100

    new fxmk stage

--- src/main/java/org/generalka/MainSceneController.java
+++ src/main/java/org/generalka/MainSceneController.java
@@ -1,12 +1,18 @@
 package org.generalka;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import javax.security.auth.Subject;
 import java.io.IOException;
 public class MainSceneController {
 
@@ -15,7 +21,9 @@ public class MainSceneController {
     }
 
     @FXML
-    private Button button;
+    private Button loginButton;
+    @FXML
+    private Button registerButton;
     @FXML
     private Label wrongLogIn;
     @FXML
@@ -23,6 +31,19 @@ public class MainSceneController {
     @FXML
     private PasswordField password;
 
+
+
+    public void moveToRegister (ActionEvent event) throws  IOException{
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/register.fxml"));
+        Parent parent = loader.load();
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Attender");
+        stage.showAndWait();
+    }
 
 
     public void userLogin(ActionEvent event) throws IOException {
